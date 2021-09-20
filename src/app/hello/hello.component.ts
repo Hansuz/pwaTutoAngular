6fa0e981d47ef11cd0ee3f5126c702905025da52
Dylan <heyraud.dylan@gmail.com>
2021-09-20 11:10:06 +0200

    Component hello

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AnyARecord } from 'dns';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+
+  movies : any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    fetch('https://ghibliapi.herokuapp.com/films/?limit=10')
+    .then((response) => response.json())
+    .then((json) => {
+      this.movies = json;
+    });
+
+  }
+
+}
